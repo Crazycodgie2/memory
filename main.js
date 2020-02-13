fcc6cb7bdca851e8ba44123d062fecb193e13124
Colin Littlejohn <crazycodgie13@gmail.com>
2020-02-13 15:15:38 -0800

    initial set up done pre-js

---
+++ main.js
@@ -0,0 +1,22 @@
+$("#card").flip()
+$("#card2").flip()
+$("#card3").flip()
+$("#card4").flip()
+$("#card5").flip()
+$("#card6").flip()
+$("#card7").flip()
+$("#card8").flip()
+$("#card9").flip()
+$("#card10").flip()
+$("#card11").flip()
+$("#card12").flip()
+$("#card13").flip()
+$("#card14").flip()
+$("#card15").flip()
+$("#card16").flip()
+$("#card17").flip()
+$("#card18").flip()
+
+if ($("#element").css("color") == "rgb(0, 0, 0)") {
+  // do something
+}
