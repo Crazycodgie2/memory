1f54cf39c64ca86d333d2c64bfaae720684a4ba3
Colin Littlejohn <crazycodgie13@gmail.com>
2020-02-13 16:27:23 -0800

    First day finish commit

--- main.js
+++ main.js
@@ -17,6 +17,24 @@ $("#card16").flip()
 $("#card17").flip()
 $("#card18").flip()
 
-if ($("#element").css("color") == "rgb(0, 0, 0)") {
-  // do something
+if ($("#element").css("color") == "silver") {
+  setActiveClass(".matched")
 }
+
+$(".matched").off("click") //disables click event
+
+// if ($("#element").css("color") == "rgb(0, 0, 0)") {
+//   // do something
+// }
+// $(".noclick").click(function(e) {
+//   if (".matched") {
+//     e.preventDefault()
+//   }
+// })
+
+// $("#id").on("click", function() {
+//enables click event
+//do your thing here
+// })
+
+// $("#id").off("click") //disables click event
